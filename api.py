a50a96c9c78b185936a40dfdf0c76b974822863d
rmachado <rodrigo.machado3.14@hotmail.com>
2019-10-15 14:46:05 -0300

    raspagem-inicial-ok

---
+++ api.py
@@ -0,0 +1,73 @@
+from collections import namedtuple
+from bs4 import BeautifulSoup as bs
+from requests import get
+
+
+ATRIBUTOS = {
+    "url_toll": "http://www.antt.gov.br/rodovias/Pedagio.html",
+    "base_url": "http://www.antt.gov.br"
+}
+
+# definindo uma namedtuple
+site = namedtuple('Antt', 'site')
+
+class Tolls:
+
+    def format_strs(self, string: str) -> str:
+        """
+        :param string: string referente a determinados atributos
+        :return: string sem espaços.
+        """
+        # remove o ':' e também os espaçoes, a partir do indice 1
+        # return string.split(':')[1].strip()
+        return string.strip()
+
+    def gen_antt_concession(self, url: str) -> str:
+        """
+        :param url: url contendo determinada lista de pedagios
+        :return: generator contendo determinada lista de concessão
+        """
+        page = get(url)
+        bs_page = bs(page.text, 'html.parser')
+        boxes = bs_page.find_all('div', {'class': 'row no-margin'})
+        for box in boxes:
+            try:
+                titles = box.find('ul').text
+                yield titles
+            except Exception as err:
+                print(err)
+                pass
+
+    def gen_links_for_concession(self, url: str) -> str:
+        """
+        :param url: pagina inicial do site antt
+        :return:lista contendo determinado links
+        """
+        page = get(url)
+        bs_page = bs(page.text, 'html.parser')
+        boxe = bs_page.find_all('div', {'class': 'row no-margin'})
+        tag = [link.find_all('a') for link in boxe][0]
+        yield [a.get('href') for a in tag]
+
+    def get_tam_and_list_page(self, url: str) -> tuple:
+        """
+        :param url: pagina inicial do site antt
+        :return: tamanho atual/paginação e tambem uma lista contendo esses elementos;
+        """
+        page = get(url)
+        bs_page = bs(page.text, 'html.parser')
+        boxe = bs_page.find_all('div', {'class': 'row no-margin'})
+        tag = [link.find_all('a') for link in boxe][0]
+        tam = len([a.get('href') for a in tag])
+        links = [a.get('href') for a in tag]
+        return tam, links
+
+tam, links = Tolls().get_tam_and_list_page(ATRIBUTOS.get('url_toll'))
+
+for link in links:
+    print(site('{}{}'.format(ATRIBUTOS.get('base_url'), Tolls().format_strs(link))))
+
+
+
+#for box  in gen_antt_concession(ATRIBUTOS.get('url_toll')):
+    #    print(box)
---
+++ app/tolls.py
@@ -0,0 +1,60 @@
+# [1] http://www.antt.gov.br/rodovias/Pedagio.html
+
+# antt | concession
+TOLL = {"Autopista Fernão Dias": {"url":"http://www.antt.gov.br/rodovias/Concessoes_Rodoviarias/Autopista_Fernao_Dias/Autopista_Fernao_Dias_Pedagio",
+                                  "localização":[
+                                      {"praça": "P01",
+                                       "km": "40,5",
+                                       "municipio": "Campos dos Goytacazes",
+                                       "rodovia": "BR101",
+                                       "uf": "RJ"
+                                       },
+                                      {
+                                       "praça": "P02",
+                                       "km": "123",
+                                       "municipio": "Campos dos Goytacazes",
+                                       "rodovia": "BR101",
+                                       "uf": "RJ"
+                                      },
+                                      {
+                                       "praça": "P03",
+                                       "km": "192,5	",
+                                       "municipio": "Casimiro de Abreu",
+                                       "rodovia": "BR101",
+                                       "uf": "RJ"
+                                      },
+                                      {
+                                       "praça": "P04",
+                                       "km": "252,5",
+                                       "municipio": "Rio Bonito",
+                                       "rodovia": "BR101",
+                                       "uf": "RJ"
+                                      },
+                                      {
+                                       "praça": "P05",
+                                       "km": "299,1",
+                                       "municipio": "São Gonçalo",
+                                       "rodovia": "BR101",
+                                       "uf": "RJ"
+                                      },
+                                  ],
+
+        "Autopista Fluminense": "http://www.antt.gov.br/rodovias/Concessoes_Rodoviarias/Autopista_Fluminense/Autopista_Fluminense_Pedagios.html",
+        "Autopista Litoral Sul": "",
+        "Autopista Planalto Sul": "",
+        "Autopista Régis Bittencourt": "",
+        "CONCEBRA": "",
+        "CONCER": "",
+        "CRT": "",
+        "ECO050": "",
+        "ECO101": "",
+        "ECOPONTE": "",
+        "ECOSUL": "",
+         "MS VIA": "",
+         "Nova Dutra": "",
+         "Rodovia do Aço": "",
+         "Rodovia Transbrasiliana": "",
+         "Rota do Oeste": "",
+         "ViaSul": "",
+         "Via 040": "",
+         "Via Bahia": "" }}
